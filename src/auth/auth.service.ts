039fea4e0fe3912abd00cfe59f86c12cf8360925
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-04-03 17:53:20 -0300

    use id

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -42,7 +42,7 @@ export class AuthService {
       const { password, email } = loginUserDto;
       const user = await this.userRepository.findOne({
         where: { email },
-        select: { email: true, password: true }
+        select: { email: true, password: true, id: true }
       });
       if (!user) {
         throw new UnauthorizedException('Credentials are not valid(email)');
